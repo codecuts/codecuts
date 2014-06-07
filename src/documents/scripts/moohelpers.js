2e7098b13c566893711fd34f53acea18644a75e9
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2014-06-07 12:49:38 +0200

    added debounce function to moohelpers

--- src/documents/scripts/moohelpers.js
+++ src/documents/scripts/moohelpers.js
@@ -46,4 +46,48 @@ module.exports = (function() {
 
     });
 
+
+	// Returns a function, that, as long as it continues to be invoked, will not
+	// be triggered. The function will be called after it stops being called for
+	// N milliseconds. If `immediate` is passed, trigger the function on the
+	// leading edge, instead of the trailing.
+	Function.implement('debounce', function(wait, immediate) {
+		var timeout, args, context, timestamp, result;
+
+		var func = this;
+
+		var later = function() {
+			var last = new Date().getTime() - timestamp;
+
+			if (last < wait && last > 0) {
+				timeout = setTimeout(later, wait - last);
+			}
+			else {
+				timeout = null;
+				if (!immediate) {
+				  result = func.apply(context, args);
+				  if (!timeout) {
+				  	context = args = null;
+				  }
+				}
+			}
+	    };
+
+		return function() {
+			context = this;
+	    	args = arguments;
+	    	timestamp = new Date().getTime();
+	    	var callNow = immediate && !timeout;
+	     	if (!timeout) {
+	    		timeout = setTimeout(later, wait);
+	    	}
+	      	if (callNow) {
+	        	result = func.apply(context, args);
+	        	context = args = null;
+	      	}
+	      	return result;
+   		};
+
+	});
+
 })();
