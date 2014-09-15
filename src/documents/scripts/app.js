158fa48b1198e83ccf715b969be0d615994ea41a
Ethan Miller <ethanzanemiller@gmail.com>
2014-09-15 20:42:44 +0200

    Added comments to app.js

--- src/documents/scripts/app.js
+++ src/documents/scripts/app.js
@@ -1,5 +1,9 @@
+/** Holds the page 'view' module instance */
 var page = require('./page.js');
 
+/**
+ * Event assignment loads the page once the DOM has loaded 
+ */ 
 window.addEvent('domready', function () {
     'use strict';
     var view = page.getInstance();
