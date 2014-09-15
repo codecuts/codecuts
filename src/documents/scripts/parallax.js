890503313b31d160dfeed730eef1503404d0053f
Ethan Miller <ethanzanemiller@gmail.com>
2014-09-15 20:56:19 +0200

    Comments added to parallax.js:

--- src/documents/scripts/parallax.js
+++ src/documents/scripts/parallax.js
@@ -1,17 +1,30 @@
+/**
+ * Parallax module enables a Parallax effect.
+ * @module parallax
+ */
 module.exports = new Class({
 
 	Implements: [Options, Events],
 
+	/** The object within which the parallax effect will function */
 	container: null,
 
+	/** The elements, designated by a .class specified in options, upon which the parallax effect will operate */
 	elements: null,
 
+	/** 
+	 * Contains key configuration options, including the container, which elements receive parallax effect, and 
+	 * the Parallax 'factor'.
+	 */
 	options: {
 		container: window,
 		parallaxedClass: 'parallaxed',
 		factor: 0.5
 	},
 
+	/** 
+	 * @constructor
+	 */
 	initialize: function(options) {
 		this.setOptions(options); 
 		this.container = document.id(this.options.container);
@@ -19,10 +32,17 @@ module.exports = new Class({
 		this.attach();
 	},
 
+	/**
+	 * Sets the class property elements, i.e. the elements that will receive parallax effect.
+	 */
 	setupElements: function() {
 		this.elements = $$('.'+this.options.parallaxedClass);
 	},
 
+	/** 
+	 * Returns a list of the parallaxed elements that are visible on the page.
+	 * @return {Array} Containing a list of the visible parallaxed elements.
+	 */
 	getVisibleElements: function() {
 		var visible = [],
 		 	scroll = this.container.getScroll(),
@@ -43,16 +63,25 @@ module.exports = new Class({
 		return visible;
 	},
 
+	/** 
+	 * 'Attaches' the Parallax scroll event to the page.
+	 */
 	attach: function() {
 		that = this;
 		that.container.addEvent('scroll', that.parallaxScroll);
 	},
 
+	/** 
+	 * 'Detaches' the Parallax scroll event from the page.
+	 */
 	detach: function() {
 		that = this;
 		that.container.removeEvent('scroll', that.parallaxScroll);
 	},
 
+	/** 
+	 * The Parallax function, called as a callback to the page's scroll event.
+	 */
 	parallaxScroll: function() {
 		var newPosY,
 			newPosX,
